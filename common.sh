33326bb8f4318f6461529530c5f90d8282c50a48
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-26 18:52:08 +0530

    shell-roboshop-common-payment

--- common.sh
+++ common.sh
@@ -63,6 +63,13 @@ java_setup(){
     VALIDATE $? "Renaming the Artifact"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+    VALIDATE $? "Installing Python3"
+    pip3 install -r requirements.txt &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
 app_setup(){
     id roboshop &>>$LOG_FILE
     if [ $? -ne 0]; then
---
+++ payment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+source ./common.sh 
+app_name=payment
+
+check_root 
+app_setup 
+python_setup 
+systemd_setup
+
+
+app_restart 
+print_total_time
