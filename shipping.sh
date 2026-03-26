f31709c0caa15882c5dd22803d33d0e24f799a2a
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-26 19:43:55 +0530

    shell-roboshop-common-shipping

--- shipping.sh
+++ shipping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+set -e
 source ./common.sh 
 app_name=shipping
 
@@ -11,10 +11,14 @@ systemd_setup
 dnf install mysql -y 
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
+VALIDATE $? "Checking root user accessing cities schema in MYSQLHOST Server"
 if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    VALIDATE $? "Creating app schema in Db"
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+    VALIDATE $? "Creating app user in Db"
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Creating masterdata in Db"
 else
     echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi 
