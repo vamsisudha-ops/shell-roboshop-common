d05bc4593a1ed373a67e4a6dc726af1af8b4124b
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-26 11:47:32 +0530

    shell-roboshop-common-catalogue

--- common.sh
+++ common.sh
@@ -13,7 +13,9 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log  --> giving full path here 
 START_TIME=$(date +%s)
+$SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws86sd.fun
+
 
 mkdir -p $LOGS_FOLDER   # -p checks already directory is there or not, if it's not automatically it create a directory
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE  #TO APPEND THE ECHO STATEMENT IN LOGS FILE
