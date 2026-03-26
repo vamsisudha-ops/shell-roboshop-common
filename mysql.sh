904eb99d488d661383eeac475f07c902483dc2b8
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-26 17:58:39 +0530

    shell-roboshop-common-mysql

---
+++ mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+source ./common.sh 
+
+check_root
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MYSQL Server"
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld  
+VALIDATE $? "Enabling and Starting MYSQL Server"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "Setting root password to MYSQL Server"
+
+print_total_time 
