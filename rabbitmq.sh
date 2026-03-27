34b2feed67153ca6a5a40e0ddebe19c457a0e987
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 19:46:14 +0530

    Catalogue

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
-source ./common.sh 
+source ./common.sh
 
 check_root
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE    ### Giving full/absolute path 
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
 VALIDATE $? "Adding RabbitMQ repo"
 dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installing RabbitMQ Server"
@@ -14,6 +14,6 @@ systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting RabbitMQ"
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
-VALIDATE $? "Setting up Permissions"
+VALIDATE $? "Setting up permissions"
 
-print_total_time 
+print_total_time
