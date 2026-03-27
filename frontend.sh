dfa91f038c6cfb54691eac906d1d6492f2a01555
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-28 01:04:10 +0530

    corrections

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,11 @@
 #!/bin/bash 
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]; then 
-    echo "ERROR:: please run this script with root previlege"
-    exit 1 #failure is other than 0 
-fi 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
